41acc293e2df79376848c39841903a46b3a2bae0
Frazier Mork <fraziermork@gmail.com>
2016-02-03 22:28:58 -0800

    added responsive styling with mobile first design

--- js/app.js
+++ js/app.js
@@ -4,12 +4,15 @@ function ProjectSummary(inputProject){
   this.publicationDate = inputProject.publicationDate;
   this.articleTitle = inputProject.articleTitle;
   this.summaryContent = inputProject.summaryContent;
+  this.iconClass = inputProject.iconClass;
+  this.category = inputProject.category;
+  this.projectImage = inputProject.projectImage;
 }
 
 ProjectSummary.prototype.returnProjectSummary = function() {
   var $newProject = $('.template').clone();
   $newProject.find('.project-title-link').attr('href', this.projectTitleLink);
-  $newProject.find('.article-title').text(this.articleTitle);
+  $newProject.find('.article-title').text(' ' + this.articleTitle).addClass(this.iconClass);
   $newProject.find('.article-content').html(this.summaryContent);
   $newProject.find('.publication-date').attr('datetime', this.publicationDate).html('<em>Project last updated about ' + parseInt( (new Date() - new Date(this.publicationDate))/60/60/24/1000 ) + ' days ago.</em>');
 
