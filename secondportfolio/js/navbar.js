f8e132f6a8ce3766182efca422e01a83f0fd2338
Frazier Mork <fraziermork@gmail.com>
2016-02-15 13:18:39 -0800

    building out test portfolio with client side routing

--- secondportfolio/js/navbar.js
+++ secondportfolio/js/navbar.js
@@ -1,21 +1,21 @@
-(function(module) {
-  var topNavBarObject = {};
-
-  topNavBarObject.handleTopNav = function() {
-    $('.top-navbar').on('click','.navbar-link', function(event) {
-      $('.page-content').hide();
-      if( $(this).attr('data-nav')) {
-        event.preventDefault();
-        var sectionId = '#' + $(this).attr('data-nav');
-        $(sectionId).fadeIn();
-        // $(sectionId).fadeIn().css('display', 'flex').css('flex-direction', 'column').css('justify-content', 'space-between').css('align-items', 'center').css('width', '100%');
-      }
-    });
-    $('.top-navbar .navbar-link:first').click();
-  };
-
-  module.topNavBarObject = topNavBarObject;
-}(window))
+// (function(module) {
+//   var topNavBarObject = {};
+//
+//   topNavBarObject.handleTopNav = function() {
+//     $('.top-navbar').on('click','.navbar-link', function(event) {
+//       $('.page-content').hide();
+//       if( $(this).attr('data-nav')) {
+//         event.preventDefault();
+//         var sectionId = '#' + $(this).attr('data-nav');
+//         $(sectionId).fadeIn();
+//         // $(sectionId).fadeIn().css('display', 'flex').css('flex-direction', 'column').css('justify-content', 'space-between').css('align-items', 'center').css('width', '100%');
+//       }
+//     });
+//     $('.top-navbar .navbar-link:first').click();
+//   };
+//
+//   module.topNavBarObject = topNavBarObject;
+// }(window))
 
 
 // $(function() {
