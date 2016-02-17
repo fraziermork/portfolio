03361aa57bd2febbb401cbe3ad7b28c39d4e0d6b
Frazier Mork <fraziermork@gmail.com>
2016-02-17 15:15:51 -0800

    comment added

--- pagejs-portfolio/js/routes.js
+++ pagejs-portfolio/js/routes.js
@@ -1,6 +1,7 @@
 page('/', indexController.index);
 page.exit('/',indexController.onExit);
 page('/:currentPageContentSection', pageContentController.index);
+// page('/:currentPageContentSection/:article', pageContentController.index, pageContentController.onArticleClick);
 page('/:currentPageContentSection/:article', pageContentController.ensurePageContent, pageContentController.onArticleClick);
 
 
